978989ecbcc2eb28a1619349d684f3d423e6fc06
danielpreys <danielpreys@yahoo.com>
2019-02-24 13:14:34 -0500

    add js file

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var port = process.env.PORT || 3000;
 
 
 
-
-
 app.get('/', function(req, res) {
 res.sendFile(__dirname +  '/views/index.html');
 });
@@ -13,6 +11,10 @@ res.sendFile(__dirname +  '/views/index.html');
 app.use("/", express.static(__dirname + "/public"));
 
 
+
+
+
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
+
