d244a90c5f303257119dfbcc5a2c1d69299739bf
danielpreys <danielpreys@yahoo.com>
2019-02-24 19:16:14 -0500

    added basix ui and started adding titles to page

--- public/app.js
+++ public/app.js
@@ -1,1 +1,29 @@
-console.log("hello");
+
+$(document).ready(function() {
+
+$("#search-btn").click(function(e){
+	e.preventDefault();
+	var searchTerm = $("#searchField").val();
+	var searchKey = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
+	
+	$.ajax({
+      type:"GET",
+      url:searchKey,
+      dataType:"json",
+      success:function(data){
+      	var arr = JSON.stringify(data);
+      	for (var i = 0; i<=data.items.length; i++){
+        $("#results").append('<h1>' + data["items"][0]["volumeInfo"]["title"] + '</h1>');
+       }     
+      },
+      error: function(showError){
+        alert("nothing here");
+      }
+   });
+	
+
+    
+});
+
+        
+	});
