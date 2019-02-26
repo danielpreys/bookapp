0199e0ae224ce6e671ba14896954144ad5854af9
danielpreys <danielpreys@yahoo.com>
2019-02-25 20:44:58 -0500

    all necessary criteria posting to page

--- public/app.js
+++ public/app.js
@@ -1,19 +1,26 @@
 
 $(document).ready(function() {
 
-$("#search-btn").click(function(e){
-	e.preventDefault();
+$("#search-btn").click(function(){
+	
 	var searchTerm = $("#searchField").val();
 	var searchKey = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
+
+	$("#results").empty();
 	
 	$.ajax({
       type:"GET",
       url:searchKey,
       dataType:"json",
       success:function(data){
-      	var arr = JSON.stringify(data);
+
       	for (var i = 0; i<=data.items.length; i++){
-        $("#results").append('<h1>' + data["items"][0]["volumeInfo"]["title"] + '</h1>');
+        $("#results").append('<h2>' + "Title: " + data["items"][i]["volumeInfo"]["title"] + '</h2>');
+        $("#results").append('<img src' + '=' + data["items"][i]["volumeInfo"]["imageLinks"]["thumbnail"] + '>');
+        $("#results").append('<p>' + "Author: " + data["items"][i]["volumeInfo"]["authors"] + '</p>');
+        $("#results").append('<p>' + "Publisher: " + data["items"][i]["volumeInfo"]["publisher"] + '</p>');
+       	$("#results").append('<br>');
+
        }     
       },
       error: function(showError){
