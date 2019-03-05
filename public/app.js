c298ec862dac19dd8d0a49dd36fce688e90b3c66
danielpreys <danielpreys@yahoo.com>
2019-03-05 18:34:14 -0500

    created responsive rows, still in progress

--- public/app.js
+++ public/app.js
@@ -6,7 +6,7 @@ $("#search-btn").click(function(){
 	var searchTerm = $("#searchField").val();
 	var searchKey = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
 
-	$("#results").empty();
+	$("#spaces").empty();
 	
 	$.ajax({
       type:"GET",
@@ -16,14 +16,35 @@ $("#search-btn").click(function(){
 
       	
       	for (var i = 0; i<=data.items.length; i++){
+      	
+      	$("#spaces").append('<div class= "col-sm-3" id = "books">' + '<h2>' + data["items"][i]["volumeInfo"]["title"] + '</h2>' +
+      		'<img src' + '=' + data["items"][i]["volumeInfo"]["imageLinks"]["thumbnail"] + '>' +
+      		'<ul>' + '<li>' + "Author: " + data["items"][i]["volumeInfo"]["authors"] + '</li>' +
+      		'<li>' + "Publisher: " + data["items"][i]["volumeInfo"]["publisher"] + '</p>' +
+      		'<li>' + '<a href =' + data["items"][i]["volumeInfo"]["infoLink"] + "target='_blank'" + '>' + "Click for more Info" + '</li>'
+         + '</ul>' + '</div>' );
+
+      		
+       
+       	$("#results").append('<br>');
+		
+
+       	/*
+		$("#spaces").append('<div class= "col">');
         $("#results").append('<h2>' + data["items"][i]["volumeInfo"]["title"] + '</h2>');
         $("#results").append('<img src' + '=' + data["items"][i]["volumeInfo"]["imageLinks"]["thumbnail"] + '>');
-        $("#results").append('<p>' + "Author: " + data["items"][i]["volumeInfo"]["authors"] + '</p>');
-        $("#results").append('<p>' + "Publisher: " + data["items"][i]["volumeInfo"]["publisher"] + '</p>');
-        $("#results").append('<p>' + '<a href =' + data["items"][i]["volumeInfo"]["infoLink"] + "target='_blank'" + '>' + "Click for more Info" + '</p>');
+        $("#results").append('<li>' + "Author: " + data["items"][i]["volumeInfo"]["authors"] + '</li>');
+        $("#results").append('<li>' + "Publisher: " + data["items"][i]["volumeInfo"]["publisher"] + '</p>');
+        $("#results").append('<li>' + '<a href =' + data["items"][i]["volumeInfo"]["infoLink"] + "target='_blank'" + '>' + "Click for more Info" + '</li>');
          "Click for more Info: "  
+         
+         $("#spaces").append('</div">');
+
        	$("#results").append('<br>');
-      
+		*/
+       	
+       	
+      	
 
        }     
       },
